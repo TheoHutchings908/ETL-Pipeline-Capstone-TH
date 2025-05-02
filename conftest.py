f1ef203c393c93440fa23c09906d8a47389251cb
TheoHutchings908 <theohutchings2002@gmail.com>
2025-05-02 14:30:32 +0100

    inital setup done

--- conftest.py
+++
@@ -1,8 +0,0 @@
-# conftest.py
-import sys, os
-
-# compute the absolute path to ./src
-SRC = os.path.join(os.path.dirname(__file__), "src")
-
-# insert it at the front of sys.path:
-sys.path.insert(0, os.path.abspath(SRC))
--- src/etl/extract/extract.py
+++ src/etl/extract/extract.py
@@ -1,4 +1,5 @@
-import os
+# import necessary libraries
+from pathlib import Path
 import pandas as pd
 import logging
 import timeit
@@ -11,31 +12,34 @@ logger = setup_logger(
     level=logging.DEBUG
 )
 
+# Set the expected performance threshold (in seconds)
 EXPECTED_PERFORMANCE = 0.0001
 
-FILE_PATH = os.path.join(
-    os.path.dirname(__file__), '../../data/car_sales_data.csv'
-)
-
-TYPE = 'csv'  
+# Set the path to the CSV file
+BASE_DIR = Path(__file__).resolve().parents[3]
+FILE_PATH = BASE_DIR / 'data' / 'car_sales_data.csv'
 
 
+TYPE = 'csv'
+
+
+# extracts data from csv
 def extract_data() -> pd.DataFrame:
     start_time = timeit.default_timer()
-    
     try:
-        sales = pd.read_csv(FILE_PATH)
+        df = pd.read_csv(FILE_PATH)
         extract_sales_execution_time = timeit.default_timer() - start_time
         log_extract_success(
             logger,
             TYPE,
-            sales.shape,
+            df.shape,
             extract_sales_execution_time,
             EXPECTED_PERFORMANCE
         )
-        return sales
+        return df
     except Exception as e:
         logger.setLevel(logging.ERROR)
         logger.error(f"Error loading {FILE_PATH}: {e}")
         raise Exception(f"Failed to load CSV file: {FILE_PATH}")
-    
+
+
